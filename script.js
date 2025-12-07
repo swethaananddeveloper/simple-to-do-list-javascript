20fd0b83e0a8bcfe5eb70ce6fce60a233895c4e1
Swetha <swetha.a1297@gmail.com>
2025-12-07 19:08:37 +0530

    Updated todo list button

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-document.querySelector('#push').onclick = functional (){
+document.querySelector('#push').onclick = function(){
 
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please enter a flask")
     }
     else{
-        document.querySelector('#task').innerHTML
-+=`
+        document.querySelector('#tasks').innerHTML
++= `
         <div class = "task">
             <span id="taskname">
 ${document.querySelector ('#newtask input').value}
 
             </span>
             <button class="delete">
+            <i class="far fa-trash-alt" aria-hidden="true">
+            </i>
+            </button>
+
             </div>
+            `;
+
+    var current_task = document.querySelectorAll(".delete");
+    for(var i=0; i<=current_task.length;i++){
+        current_task[i].onclick = function(){
+            this.parentNode.remove();
+        }
     }
+    
+var task = document.querySelectorAll(".task");
+for(var i=0; i<tasks.length;i++){
+    tasks[i].onclick =function(){
+        this.classList.toggle('completed');
     }
+}
+
+document.querySelector("#newtask input").value = "";
+    }
+}
